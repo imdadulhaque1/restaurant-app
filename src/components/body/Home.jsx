e66778bd51ebb7a8c637dbf96baacb5020a2a0a3
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-03-11 16:21:11 +0600

    Dispatching Action from Component

--- src/components/body/Home.jsx
+++ src/components/body/Home.jsx
@@ -2,15 +2,21 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 
 const mapStateToProps = state =>{
-    console.log("mapStateToProps: ",state);
+    // console.log("mapStateToProps: ",state);
     return{
         a: state.dishes
     }
 }
 class Home extends Component{
     componentDidMount(){
-        console.log("Home State: ",this.state);
         console.log("Home Props: ",this.props);
+        this.props.dispatch({
+            type: 'TEST',
+            str: "Imdadul Haque"
+        })
+    }
+    componentDidUpdate(){
+        console.log("Home props Updated: ", this.props);
     }
     render(){
         document.title = 'Restaurant';
@@ -22,4 +28,4 @@ class Home extends Component{
     }
 }
 
-export default connect(mapStateToProps) (Home);
+export default connect(mapStateToProps)(Home);
