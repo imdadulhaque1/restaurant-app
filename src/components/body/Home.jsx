85eebd054e66cc24cedacbf0c989fa80091c8e08
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-03-12 21:57:56 +0600

    Creating CommentForm

--- src/components/body/Home.jsx
+++ src/components/body/Home.jsx
@@ -1,23 +1,5 @@
 import React, {Component} from 'react';
-import { connect } from 'react-redux';
-
-const mapStateToProps = state =>{
-    // console.log("mapStateToProps: ",state);
-    return{
-        a: state.dishes
-    }
-}
 class Home extends Component{
-    componentDidMount(){
-        console.log("Home Props: ",this.props);
-        this.props.dispatch({
-            type: 'TEST',
-            str: "Imdadul Haque"
-        })
-    }
-    componentDidUpdate(){
-        console.log("Home props Updated: ", this.props);
-    }
     render(){
         document.title = 'Restaurant';
         return(
@@ -28,4 +10,4 @@ class Home extends Component{
     }
 }
 
-export default connect(mapStateToProps)(Home);
+export default Home;
