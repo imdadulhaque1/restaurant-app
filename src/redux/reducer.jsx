98d653f2d95c8ce2250de31da0b493d87e1d5b27
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-03-13 16:58:21 +0600

    Switching Action Types

--- src/redux/reducer.jsx
+++ src/redux/reducer.jsx
@@ -1,20 +1,27 @@
 import DISHES from "../data/dishes";
 import COMMENTS from "../data/comments";
 import {combineReducers} from "redux";
+import * actionTypes from './actionTypes';
 
 const dishReducer = (dishState = DISHES, action) =>{
-    return dishState;
+    switch (action.type){
+        default:
+            return dishState;
+    }
 }
 const commentReducer = (commentState = COMMENTS, action) =>{
-    if(action.type === 'ADD_COMMENT'){
-        let comment = action.payload;
-        comment.id = commentState.length;
-        comment.date = new Date().toDateString();
-        console.log(comment);
-        return commentState.concat(comment);
+    switch (action.type){
+        case actionTypes.ADD_COMMENT:
+            let comment = action.payload;
+            comment.id = commentState.length;
+            comment.date = new Date().toDateString();
+            console.log(comment);
+            return commentState.concat(comment);
+        default:
+            return commentState;
     }
 
-    return commentState;
+    
 }
 
 export const Reducer = combineReducers({
